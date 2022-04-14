7cf50057cf70b077adaad513cf79ab38aae5be5d
Mitchel Kelonye <vbstreetz@gmail.com>
2022-04-14 14:54:07 +0300

    feat: index exporter

---
+++ js/index.ts
@@ -0,0 +1,4 @@
+export { Comptoir } from './comptoir';
+export { Collection } from './collection';
+export { COMPTOIR_PROGRAM_ID } from './constant';
+export { Comptoir as ComptoirDefinition } from './types/comptoir';
