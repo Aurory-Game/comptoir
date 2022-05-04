88ae9a01d71ea4b02bd873897499ea205adc90dd
ceciEstErmat <ceciestermat@gmail.com>
2022-05-04 01:13:43 +0100

    export devnet comptoir id

--- js/constant.ts
+++ js/constant.ts
@@ -1,5 +1,9 @@
 import { PublicKey } from '@solana/web3.js';
 
+export const COMPTOIR_PROGRAM_ID_DEVNET: PublicKey = new PublicKey(
+    'FCoMPzD3cihsM7EBSbXtorF2yHL4jJ6vrbWtdVaN7qZc'
+);
+
 export const COMPTOIR_PROGRAM_ID: PublicKey = new PublicKey(
   'comp8FLpN5bbNUC2fhnkFtM7T16DWvaHs8N5dcaoZWP'
 );
--- js/index.ts
+++ js/index.ts
@@ -1,6 +1,6 @@
 export { Comptoir } from './comptoir';
 export { Collection } from './collection';
-export { COMPTOIR_PROGRAM_ID } from './constant';
+export { COMPTOIR_PROGRAM_ID, COMPTOIR_PROGRAM_ID_DEVNET } from './constant';
 export type { Comptoir as ComptoirDefinition } from './types/comptoir';
 export * from './getPDAs';
 export * from './metaplex';
